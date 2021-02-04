2151975097e32f1a53f896b0e661e804d82caba1
Mathias Jørgensen <mathijo@stud.ntnu.no>
2021-02-04 11:16:14 +0100

    First commit

---
+++ src/main/java/idata2001/ch10/networkv1/MessagePost.java
@@ -0,0 +1,54 @@
+package idata2001.ch10.networkv1;
+
+import java.util.ArrayList;
+
+/**
+ * This class stores information about a post in a social network. 
+ * The main part of the post consists of a (possibly multi-line)
+ * text message. Other data, such as author and time, are also stored.
+ * 
+ * @author Michael Kölling and David J. Barnes
+ * @version 0.1
+ */
+public class MessagePost extends Post
+{
+
+    private String message;   // an arbitrarily long, multi-line message
+
+
+    /**
+     * Constructor for objects of class MessagePost.
+     * 
+     * @param author    The username of the author of this post.
+     * @param text      The text of this post.
+     */
+    public MessagePost(String author, String text)
+    {
+        super(author);
+        message = text;
+    }
+
+    public void display() {
+        System.out.println("Printing from MessagePost...");
+    }
+
+
+
+
+
+    /**
+     * Return the text of this post.
+     * 
+     * @return The post's text.
+     */
+    public String getText()
+    {
+        return message;
+    }
+
+
+
+
+    
+
+}
---
+++ src/main/java/idata2001/ch10/networkv1/NewsFeed.java
@@ -0,0 +1,77 @@
+package idata2001.ch10.networkv1;
+
+import java.util.ArrayList;
+
+/**
+ * The NewsFeed class stores news posts for the news feed in a social network 
+ * application.
+ * 
+ * Display of the posts is currently simulated by printing the details to the
+ * terminal. (Later, this should display in a browser.)
+ * 
+ * This version does not save the data to disk, and it does not provide any
+ * search or ordering functions.
+ * 
+ * @author Michael Kölling and David J. Barnes
+ * @version 0.1
+ */
+public class NewsFeed
+{
+    //private ArrayList<MessagePost> messages;
+    //private ArrayList<PhotoPost> photos;
+
+    private ArrayList<Post> posts;
+
+    /**
+     * Construct an empty news feed.
+     */
+    public NewsFeed()
+    {
+        //messages = new ArrayList<>();
+        //photos = new ArrayList<>();
+
+        this.posts = new ArrayList<>();
+    }
+
+    /**
+     * Add a text post to the news feed.
+     * 
+     * @param text  The text post to be added.
+     */
+    public void addPost(Post post )
+    {
+        posts.add(post);
+    }
+
+
+    /**
+     * Show the news feed. Currently: print the news feed details to the
+     * terminal. (To do: replace this later with display in web browser.)
+     */
+    public void show()
+    {
+        // display all text posts
+        for(Post post : this.posts) {
+            post.display();
+            System.out.println();   // empty line between posts
+        }
+
+    }
+
+    public static void main(String[] args) {
+        NewsFeed newsFeed = new NewsFeed();
+
+        MessagePost messagePost = new MessagePost("Arne", "God morgen!!");
+        //Post messagePost = new MessagePost("Arne", "God morgen!!");
+
+        System.out.println(messagePost.getText());
+        System.out.println(messagePost.getTimeStamp());
+
+        newsFeed.addPost(messagePost);
+
+        PhotoPost photoPost = new PhotoPost("Jens", "Winter.jpg", "Wonderful winter holliday!");
+        newsFeed.addPost(photoPost);
+
+        newsFeed.show();
+    }
+}
